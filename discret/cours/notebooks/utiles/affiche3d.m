6355beb7925dc281fb2fea08bb8aff97126716cc
Pascal Acco <pacco@spacer.laas.fr>
2022-03-28 11:26:00 +0200

    orthographe

--- discret/cours/notebooks/utiles/affiche3d.m
+++ discret/cours/notebooks/utiles/affiche3d.m
@@ -18,21 +18,31 @@ function affiche3d(t,s,j,W)
     xlabel('temps')
     ylabel('reel');
     zlabel('imaginaires')
-    subplot(2,2,1);
+ 
+ subplot(2,2,1);
     for id_n = 1:N
         zoo = 1 + 0.1* j(id_n);
         plot(zoo*real(s(:,id_n)),zoo*imag(s(:,id_n)),couleurs(id_n));
         hold on; box off;
-        plot3(zoo*real(W(:,id_n)),zoo*imag(W(:,id_n)),[couleurs(id_n),'o']);
+        plot(zoo*real(W(:,id_n)),zoo*imag(W(:,id_n)),[couleurs(id_n),'o']);
     end
+    xlabel('reel')
+    ylabel('imaginaire');
+    
     subplot(2,2,4);
     for id_n = 1:N
         plot(t,real(s(:,id_n)),[couleurs(id_n),'--']); hold on; box off;
         stem(j,real(W(:,id_n)),couleurs(id_n));
     end
+    xlabel('temps')
+    ylabel('reel');
+    
     subplot(2,2,3);
     for id_n = 1:N
         plot(t,imag(s(:,id_n)),[couleurs(id_n),'--']); hold on; box off;
         stem(j,imag(W(:,id_n)),couleurs(id_n));
     end
+    xlabel('temps')
+    ylabel('imaginaire');
+    
 end
