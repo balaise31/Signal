4f104c9f3b76d0387485c48e33a48fcf0279eaa7
Pascal Acco <pacco@spacer.laas.fr>
2022-05-15 22:18:05 +0200

    Mennage et séparation TP1_bili et TP2_overtone

--- installation/maj_signal.sh
+++ installation/maj_signal.sh
@@ -2,8 +2,8 @@ DEPOT="/mnt/commetud/3eme Annee IMACS/Signal"
 CIBLE="$HOME/signal_local"
 
 cd $CIBLE
-
-rsync -abv --suffix=_sauvegarde.ipynb "$DEPOT"/discret/tp/ ./tp/
-rsync -abv --suffix=_sauvegarde.ipynb "$DEPOT"/discret/td/ ./td/
+SUFFIXE=sauvegarde_du_"$(date +%d_%h_%Hh%Mm%Ss)".ipynb
+rsync -abv --suffix="$SUFFIXE" "$DEPOT"/discret/tp/ ./tp/
+rsync -abv --suffix="$SUFFIXE" "$DEPOT"/discret/td/ ./td/
 
 cp -f "$DEPOT"/installation/*.sh .
