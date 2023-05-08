625ce5b943c90302c225899c6c328e9734aa5f1f
Pascal Acco <pacco@spacer.laas.fr>
2023-05-08 10:00:10 +0200

    Navigaton avec menus

--- installation/mise_a_jour.sh
+++ installation/mise_a_jour.sh
@@ -12,6 +12,17 @@ cp -f "$DEPOT"/installation/mise_a_jour.sh ./
 cp -f "$DEPOT"/installation/*vieux*.sh ./
 cp -f "$DEPOT"/installation/README.ipynb .
 
+if [ -d "$DEPOT"/$DISCONT/cours/notebooks/ ] ;
+then
+    test ! -d cours && mkdir cours
+    test ! -d cours/notebooks  && mkdir ./cours/notebooks && cp "$DEPOT"/$DISCONT/cours/notebooks/  
+    rsync -ab --suffix="$SUFFIXE" "$DEPOT"/$DISCONT/cours/notebooks/ ./cours/notebooks/
+fi
+if [ -d "$DEPOT"/$DISCONT/defis ] ;
+then
+    test ! -d defis && mkdir defis
+    rsync -ab --suffix="$SUFFIXE" "$DEPOT"/$DISCONT/defis/ ./defis/
+fi
 echo "______________________________________"
 echo "Des fichiers ont peut-être été mis à jour"
 echo " et les anciennes versions sont sauvegardées avec le suffixe :"
