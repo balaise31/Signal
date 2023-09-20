bdca28e7da97109c30aed33bf5ecc4915c4fa2d9
Pascal Acco <pacco@spacer.laas.fr>
2023-09-20 16:19:05 +0200

    pip env

---
+++ installation/install_venv.sh
@@ -0,0 +1,6 @@
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user virtualenv
+python3 -m venv env
+. ./env/bin/activate
+python3 -m pip install  --upgrade pip
+python3 -m pip install  -r requirements.txt
